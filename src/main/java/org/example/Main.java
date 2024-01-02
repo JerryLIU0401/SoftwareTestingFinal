42aa6be434c7df58e5953678e24afc644d77c225
JerryLIU <jerry2003.0401@gmail.com>
2024-01-02 15:07:00 +0800

    plus / minus update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,6 @@ import javax.swing.*;
 
 public class Main {
   public static void main(String[] args) {
-    MainFrame mainFrame = new MainFrame();
-    mainFrame.frameInit();
-    mainFrame.setVisible(true);
-    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
   }
 }
--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Player {
+  private int id;
   private double points;
   private double orb; // offensive rebounds
   private double drb; // defensive rebounds
@@ -8,34 +9,113 @@ public class Player {
   private double blocks; // 火鍋
   private double steals; // 抄截
   private double turnovers; // 失誤
+  private double pf; // 犯規
   private double tpa; // 3-pointer attempts
   private double tpm; // 3-pointer made
   private double fga; // field goal attempts
   private double fgm; // field goal made
   private double fta; // free throw attempts
   private double ftm; // free throw made
+  private int plusminus; // plus / minus
 
-  public Player(double points, double orb, double drb, double assists,
-                double blocks, double steals, double turnovers,
-                double tpa, double tpm, double fga, double fgm, double fta, double ftm) {
-    this.points = points;
-    this.orb = orb;
-    this.drb = drb;
-    this.assists = assists;
-    this.blocks = blocks;
-    this.steals = steals;
-    this.turnovers = turnovers;
-    this.tpa = tpa;
-    this.tpm = tpm;
-    this.fga = fga;
-    this.fgm = fgm;
-    this.fta = fta;
-    this.ftm = ftm;
+  public Player(int id) {
+    this.id = id;
   }
 
-  // ft
-  public Player(){
+  public int getId() {
+    return id;
+  }
 
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public double getPoints() {
+    return points;
+  }
+
+  public void setPoints(double points) {
+    this.points = points;
+  }
+
+  public double getOrb() {
+    return orb;
+  }
+
+  public void setOrb(double orb) {
+    this.orb = orb;
+  }
+
+  public double getDrb() {
+    return drb;
+  }
+
+  public void setDrb(double drb) {
+    this.drb = drb;
+  }
+
+  public double getAssists() {
+    return assists;
+  }
+
+  public void setAssists(double assists) {
+    this.assists = assists;
+  }
+
+  public double getBlocks() {
+    return blocks;
+  }
+
+  public void setBlocks(double blocks) {
+    this.blocks = blocks;
+  }
+
+  public double getSteals() {
+    return steals;
+  }
+
+  public void setSteals(double steals) {
+    this.steals = steals;
+  }
+
+  public double getTurnovers() {
+    return turnovers;
+  }
+
+  public void setTurnovers(double turnovers) {
+    this.turnovers = turnovers;
+  }
+
+  public double getTpa() {
+    return tpa;
+  }
+
+  public void setTpa(double tpa) {
+    this.tpa = tpa;
+  }
+
+  public double getTpm() {
+    return tpm;
+  }
+
+  public void setTpm(double tpm) {
+    this.tpm = tpm;
+  }
+
+  public double getFga() {
+    return fga;
+  }
+
+  public void setFga(double fga) {
+    this.fga = fga;
+  }
+
+  public double getFgm() {
+    return fgm;
+  }
+
+  public void setFgm(double fgm) {
+    this.fgm = fgm;
   }
 
   public void setFta(double fta) {
@@ -45,6 +125,37 @@ public class Player {
   public void setFtm(double ftm) {
     this.ftm = ftm;
   }
+
+  public double getFta() {
+    return fta;
+  }
+
+  public double getFtm() {
+    return ftm;
+  }
+
+  public double getPf() {
+    return pf;
+  }
+
+  public void setPf(double pf) {
+    this.pf = pf;
+  }
+
+  // plus / minus
+  public void setPlusminus(int score) {
+    plusminus += score;
+  }
+
+  public int getPlusminus() {
+    return plusminus;
+  }
+
+  /*
+  -----------------
+   After game data
+  -----------------
+*/
 
   // Effective Field-Goal Percentage
   public double eFG() {
@@ -67,12 +178,12 @@ public class Player {
   }
 
   // 3-points percentage
-  public double tfg(){
+  public double tfg() {
     return tpa / tpm;
   }
 
   // efficiency
-  public double eff(){
+  public double eff() {
     return (points + drb + orb + assists + steals + blocks)
             - (fgm - fga) + (ftm - fta) + turnovers;
   }
